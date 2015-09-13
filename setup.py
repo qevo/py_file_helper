23984ac43f0ffa50bf0ba6ca52b621285d5536dc
Qevo <qevo@qevonics.net>
2015-09-13 10:18:28 -0500

    fix dependency links

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 config = {}
 config["name"]             = "file_helper"
-config["version"]          = "0.1.1"
+config["version"]          = "0.1.2"
 config["author"]           = "Qevo"
 config["author_email"]     = "qevo@qevonics.net"
 config["description"]      = ("File search, identification, analysis, and manipulation tools.")
@@ -20,8 +20,9 @@ config["license"]          = "MIT"
 config["keywords"]         = "file_system file_management files file"
 config["url"]              = "https://github.com/qevo/py_file_helper"
 config["packages"]         = ["file_helper"]
-config["install_requires"] = ['data_helper']
-config["long_description"] = read('README.md')
+config["install_requires"] = ["data_helper"]
+config["dependency_links"] = ["git+ssh://git@github.com:qevo/py_data_helper.git"]
+config["long_description"] = read("README.md")
 config["classifiers"]      = [
     "Development Status :: 3 - Alpha",
     "Topic :: Utilities",
